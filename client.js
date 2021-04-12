6c46b882c0aeb6f16d0e9001cf02e5fe91b8c9e4
Christian Back <cdirt27@gmail.com>
2021-04-12 01:44:54 -0500

    Created function called PushEmployeeInfo which takes input data and pushes the input information into an array as an object, and clears input fields after submission

--- client.js
+++ client.js
@@ -1,6 +1,34 @@
 console.log('This is JS');
 
+let employees = [];
+
 $(document).ready(function () {
-    console.log('Hi I\'m jQuery');
-    $('body').append();
+    // console.log('Hi I\'m jQuery');
+    // $('body').append();
+
+$('#uploadData').on('click', pushEmployeeInfo);
+
 });
+
+
+function pushEmployeeInfo (){
+    //Get Data from Input Fields
+    employeeData = {
+        firstName: $('#firstName').val(),
+        lastName: $('#lastName').val(),
+        ID: $('#ID').val(),
+        Position: $('#Position').val(),
+        annualSalary: $('#annualSalary').val(),
+    }
+    //Move Objects into Global Array
+    employees.push(employeeData);
+
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#ID').val('');
+    $('#Position').val('');
+    $('#annualSalary').val('');
+
+    console.log(employees);
+}
+
