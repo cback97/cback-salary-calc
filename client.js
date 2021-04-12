a27bdac672bd0c883b2dc0679b0a414a95749080
Christian Back <cdirt27@gmail.com>
2021-04-12 02:13:46 -0500

    Appending employee data in rows on the DOM

--- client.js
+++ client.js
@@ -6,17 +6,17 @@ $(document).ready(function () {
     // console.log('Hi I\'m jQuery');
     // $('body').append();
 
-$('#uploadData').on('click', pushEmployeeInfo);
+    $('#uploadData').on('click', pushEmployeeInfo);
 
 });
 
 
-function pushEmployeeInfo (){
+function pushEmployeeInfo() {
     //Get Data from Input Fields
     employeeData = {
         firstName: $('#firstName').val(),
         lastName: $('#lastName').val(),
-        ID: $('#ID').val(),
+        id: $('#ID').val(),
         Position: $('#Position').val(),
         annualSalary: $('#annualSalary').val(),
     }
@@ -30,5 +30,26 @@ function pushEmployeeInfo (){
     $('#annualSalary').val('');
 
     console.log(employees);
+    displayOutput();
 }
 
+function displayOutput() {
+    // Clear everything out first
+    $('#employeeOutput').empty();
+
+    // Loop over array & add a row to the table on the DOM
+    for (member of employees) {
+        $('#employeeOutput').append(`
+            <tr>
+                <td>${member.firstName}</td>
+                <td>${member.lastName}</td>
+                <td>${member.id}</td>
+                <td>${member.Position}</td>
+                <td>${member.annualSalary}</td>
+                <td>
+                    <button class="delete">Delete</button>
+                </td>
+            </tr>
+        `)
+    }
+}
